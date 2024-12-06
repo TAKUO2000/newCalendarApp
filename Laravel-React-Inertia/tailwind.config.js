93b23707ad39f0be018b4663b921030d7bec0374
Kyoya Takumu <kyoyatakumu@gmail.com>
2024-12-06 20:02:35 +0900

    各種ライブラリ導入

--- Laravel-React-Inertia/tailwind.config.js
+++ Laravel-React-Inertia/tailwind.config.js
@@ -1,19 +1,29 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                body: ["Graphik", "sans-serif"],
+            },
+            fontSize: {
+                base: [
+                    "1rem",
+                    { lineHeight: "1.5rem", letterSpacing: "0.03em" },
+                ],
+                title: [
+                    "1.5rem",
+                    { lineHeight: "2rem", letterSpacing: "0.03em" },
+                ],
             },
         },
     },
